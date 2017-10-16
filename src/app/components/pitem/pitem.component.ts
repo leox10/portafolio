8b0ada96339e0f05e95f68bfbf4aee8800322afa
Leonardo <jleogo9@gamil.com>
2017-10-16 16:22:21 -0500

    Item

---
+++ src/app/components/pitem/pitem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pitem',
+  templateUrl: './pitem.component.html'
+})
+export class PitemComponent {
+
+  constructor() { }
+
+}
