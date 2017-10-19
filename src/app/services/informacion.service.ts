336d4e16bee8a2284834ef784c1d48dfa6a7436e
Leonardo <jleogo9@gamil.com>
2017-10-19 16:59:03 -0500

    servicio firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,14 +6,29 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_nosotros:boolean = false;
+  equipo:any[]=[];
 
   constructor(public http:Http)
   {
+    this.carga_info();
+    this.carga_nosotros();
+  }
+
+  public carga_info()
+  {
     this.http.get("assets/data/info.pagina.json").subscribe(data => {
-      console.log(data.json());
+      //console.log(data.json());
       this.cargada = true;
       this.info = data.json();
     })
   }
-
+  public carga_nosotros()
+  {
+    this.http.get("https://tienda-b9dda.firebaseio.com/equipo.json").subscribe(data => {
+      console.log(data.json());
+      this.cargada_nosotros = true;
+      this.equipo = data.json();
+    })
+  }
 }
